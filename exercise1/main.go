3b4aef57925e5567b1d79e655053a5f9035166dc
Your Name <you@example.com>
2026-01-09 15:17:58 +0800

    可以查询default namespace下所有pod

--- exercise1/main.go
+++ exercise1/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"path/filepath"
 
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -27,4 +31,18 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	// 列出default ns下的Pod名称和状态
+	fmt.Printf("Listing pods in namespace %q:\n", apiv1.NamespaceDefault)
+
+	podsClient := clientset.CoreV1().Pods(apiv1.NamespaceDefault)      // 创建podsClient
+	list, err := podsClient.List(context.TODO(), metav1.ListOptions{}) // List Pod
+	if err != nil {
+		panic(err)
+	}
+
+	for _, d := range list.Items {
+		fmt.Printf("* Pod name: %s\n", d.Name)
+	}
+
 }
